fbfad1c926fbcef71d866bfc9e0cefeb1f798933
iri-na <imcarcam@uwaterloo.ca>
2022-10-18 05:00:12 -0400

    Create CartItem Component

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,15 +1,23 @@
-import { Offcanvas, OffcanvasHeader } from "react-bootstrap";
+import { Offcanvas, OffcanvasHeader, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { CartItem } from "./CartItem";
 
 type ShoppingCartProps = {
     isOpen: boolean
 }
 
 export function ShoppingCart({ isOpen }: ShoppingCartProps) {
-    const { closeCart } = useShoppingCart();
+    const { closeCart, cartItems } = useShoppingCart();
     return <Offcanvas show={isOpen} onHide={closeCart} placement="end">
         <OffcanvasHeader closeButton>
             <Offcanvas.Title>Cart</Offcanvas.Title>
         </OffcanvasHeader>
+        <Offcanvas.Body>
+            <Stack gap={3}>
+                {cartItems.map(item => (
+                    <CartItem key={item.id} {...item} />
+                ))}
+            </Stack>
+        </Offcanvas.Body>
     </Offcanvas>
 }
