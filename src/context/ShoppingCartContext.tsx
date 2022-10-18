0a333e812178389ba44ff72feca8698827b4b6bf
iri-na <imcarcam@uwaterloo.ca>
2022-10-18 05:19:15 -0400

    Create useLocalStorage Hook To Persist Local Storage

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode, useContext, useState } from "react";
-
 import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type ShoppingCartProviderProps = {
     children: ReactNode;
@@ -29,8 +29,8 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
-    const [cartItems, setCartItems] = useState<CartItem[]>([]);
     const [isOpen, setIsOpen] = useState(false);
+    const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart", []);
 
     const cartQuantity = cartItems.reduce((quantity, item) => item.quantity + quantity, 0);
 
